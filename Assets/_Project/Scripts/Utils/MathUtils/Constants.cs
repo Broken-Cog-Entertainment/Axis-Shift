5d40c0723d3d54489fa84bc7e305caf7a45b2fc0
Alvin Philips <alvinphilips257@gmail.com>
2025-03-21 11:37:08 -0400

    feat: objective marker

---
+++ Assets/_Project/Scripts/Utils/MathUtils/Constants.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace AS.Utils.MathUtils
+{
+    public static class Constants
+    {
+        public const float PI = Mathf.PI;
+        public const float TAU = PI * 2;
+        public const float PI_BY_2 = PI * 0.5f;
+    }
+}
